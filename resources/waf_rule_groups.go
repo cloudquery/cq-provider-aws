b2bdaf74db616da4c0602b762d06fab8593c20f9
Michel Vocks <michel@cloudquery.io>
2021-06-07 17:46:26 +0200

    Fix merge issue

--- resources/waf_rule_groups.go
+++ resources/waf_rule_groups.go
@@ -48,6 +48,14 @@ func WafRuleGroups() *schema.Table {
 				Name: "rule_group_id",
 				Type: schema.TypeString,
 			},
+			{
+				Name: "metric_name",
+				Type: schema.TypeString,
+			},
+			{
+				Name: "name",
+				Type: schema.TypeString,
+			},
 		},
 	}
 }
