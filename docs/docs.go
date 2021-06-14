0923000c75a42b5a9d9be7a1496af06fdbab679e
roneli <38083777+roneli@users.noreply.github.com>
2021-06-14 16:54:27 +0300

    upgrade sdk to 0.2.6

--- docs/docs.go
+++ docs/docs.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/cloudquery/cq-provider-aws/resources"
 	"github.com/cloudquery/cq-provider-sdk/provider/docs"
 )
 
 func main() {
-	docs.GenerateDocs(resources.Provider(), "./docs")
+	err := docs.GenerateDocs(resources.Provider(), "./docs")
+	if err != nil {
+		fmt.Sprintf("Failed to geneerate docs: %s", err)
+	}
 }
