d290917e21fea031320ae59550814cf9f49b5431
Itay Zagron <zagron@gmail.com>
2021-08-18 14:37:14 +0200

    added route53

---
+++ resources/integration_tests/aws_route53_health_check_test.go
@@ -0,0 +1,27 @@
+package integration_tests
+
+import (
+	"fmt"
+	"github.com/Masterminds/squirrel"
+	"github.com/cloudquery/cq-provider-aws/resources"
+	providertest "github.com/cloudquery/cq-provider-sdk/provider/testing"
+	"testing"
+)
+
+func TestIntegrationRoute53HealthChecks(t *testing.T) {
+	awsTestIntegrationHelper(t, resources.Route53HealthChecks(), func(res *providertest.ResourceIntegrationTestData) providertest.ResourceIntegrationVerification {
+		return providertest.ResourceIntegrationVerification{
+			Name: "aws_route53_health_checks",
+			Filter: func(sq squirrel.SelectBuilder, res *providertest.ResourceIntegrationTestData) squirrel.SelectBuilder {
+				return sq.Where("fully_qualified_domain_name = ?", fmt.Sprintf("%s%s.com", res.Prefix, res.Suffix))
+			},
+			ExpectedValues: []providertest.ExpectedValue{{
+				Count: 1,
+				Data: map[string]interface{}{
+					"resource_path": "/",
+					"request_interval": 10.0,
+				},
+			}},
+		}
+	})
+}
