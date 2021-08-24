1df49a560a2d4a179203af2dc388d0c99375f8d4
Timur Irmatov <irmatov@gmail.com>
2021-08-24 13:49:03 +0500

    Add waf web acls e2e test

---
+++ resources/integration_tests/infra/aws_waf_web_acls.tf
@@ -0,0 +1,43 @@
+resource "aws_waf_ipset" "waf_ipset_2" {
+  name = "waf_ipset_2"
+
+  ip_set_descriptors {
+    type  = "IPV4"
+    value = "192.0.7.0/24"
+  }
+}
+
+resource "aws_waf_rule" "waf_rule_2" {
+  depends_on  = [aws_waf_ipset.waf_ipset_2]
+  name        = "waf_rule_2"
+  metric_name = "wafrule2"
+
+  predicates {
+    data_id = aws_waf_ipset.waf_ipset_2.id
+    negated = false
+    type    = "IPMatch"
+  }
+}
+
+resource "aws_waf_web_acl" "waf_web_acl_1" {
+  depends_on = [
+    aws_waf_ipset.waf_ipset_2,
+    aws_waf_rule.waf_rule_2,
+  ]
+  name        = "waf_web_acl_1"
+  metric_name = "wafwebacl1"
+
+  default_action {
+    type = "ALLOW"
+  }
+
+  rules {
+    action {
+      type = "BLOCK"
+    }
+
+    priority = 1
+    rule_id  = aws_waf_rule.waf_rule_2.id
+    type     = "REGULAR"
+  }
+}
