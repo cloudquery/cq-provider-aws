6f1bd316c928a6cdb4d4d43aec99290c1efcc66b
Andrii Romanenko <andrejjrom@gmail.com>
2021-08-27 16:50:31 +0300

    some more ec2 resources added

--- resources/integration_tests/aws_ec2_ebs_volumes_test.go
+++ resources/integration_tests/aws_ec2_ebs_volumes_test.go
@@ -1,33 +1,36 @@
 package integration_tests
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/cloudquery/cq-provider-aws/resources"
 	providertest "github.com/cloudquery/cq-provider-sdk/provider/testing"
 )
 
-func TestIntegrationEc2VpcEndpoints(t *testing.T) {
-	awsTestIntegrationHelper(t, resources.Ec2VpcEndpoints(), []string{"aws_ec2_vpc.tf", "aws_vpc.tf"}, func(res *providertest.ResourceIntegrationTestData) providertest.ResourceIntegrationVerification {
+func TestIntegrationEc2EbsVolumes(t *testing.T) {
+	awsTestIntegrationHelper(t, resources.Ec2EbsVolumes(), nil, func(res *providertest.ResourceIntegrationTestData) providertest.ResourceIntegrationVerification {
 		return providertest.ResourceIntegrationVerification{
-			Name: "aws_ec2_vpc_endpoints",
+			Name: resources.Ec2EbsVolumes().Name,
 			//Filter: func(sq squirrel.SelectBuilder, res *providertest.ResourceIntegrationTestData) squirrel.SelectBuilder {
 			//	return sq.Where(squirrel.And{
 			//		squirrel.Eq{"tags->>'TestId'": res.Suffix},
-			//		squirrel.NotEq{"status_code": "deleted"},
+			//		squirrel.NotEq{"state_name": "terminated"},
 			//	})
 			//},
 			ExpectedValues: []providertest.ExpectedValue{
 				{
 					Count: 1,
 					Data: map[string]interface{}{
-						"vpc_endpoint_type":   "Gateway",
-						"requester_managed":   false,
-						"private_dns_enabled": false,
+						"volume_type":          "gp2",
+						"multi_attach_enabled": false,
+						"encrypted":            false,
+						"fast_restored":        false,
+						"size":                 float64(5),
 						"tags": map[string]interface{}{
-							"Type":        "integration_test",
-							"Environment": "test",
-							"TestId":      res.Suffix,
+							"Type":   "integration_test",
+							"TestId": res.Suffix,
+							"Name":   fmt.Sprintf("ebs-%s%s", res.Prefix, res.Suffix),
 						},
 					},
 				},
--- resources/integration_tests/infra/aws_ec2_vpc.tf
+++ resources/integration_tests/infra/aws_ec2_vpc.tf
@@ -22,11 +22,91 @@ resource "aws_vpc" "aws_ec2_vpc_peering_vpc" {
 }
 
 resource "aws_vpc_endpoint" "aws_ec2_vpc_endpoint" {
-  vpc_id       = aws_vpc.aws_ec2_vpc_peering_vpc.id
+  vpc_id = aws_vpc.aws_ec2_vpc_peering_vpc.id
   service_name = "com.amazonaws.${data.aws_region.current.name}.s3"
 
   tags = {
     Environment = "test"
+  }
+}
+
+resource "aws_ec2_transit_gateway" "aws_ec2_vpc_endpoint_tg" {
+  description = "description ${var.test_prefix}-${var.test_suffix}"
+}
+
+
+resource "aws_security_group" "aws_ec2_vpc_security_group" {
+  name = "${var.test_prefix}${var.test_suffix}-sg"
+  vpc_id = aws_vpc.aws_vpc.id
+
+  ingress {
+    protocol = "tcp"
+    from_port = "80"
+    to_port = "80"
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+    ipv6_cidr_blocks = [
+      "::/0"]
+  }
+}
+
+resource "aws_network_acl" "aws_ec2_vpc_network_acl" {
+  vpc_id = aws_vpc.aws_vpc.id
+
+  egress = [
+    {
+      protocol = "tcp"
+      rule_no = 200
+      action = "allow"
+      cidr_block = "10.3.0.0/18"
+      from_port = 443
+      to_port = 443
+      icmp_code = 0
+      icmp_type = 0
+      ipv6_cidr_block = ""
+    }
+  ]
+
+  ingress = [
+    {
+      protocol = "tcp"
+      rule_no = 100
+      action = "allow"
+      cidr_block = "10.3.0.0/18"
+      from_port = 80
+      to_port = 80
+      icmp_code = 0
+      icmp_type = 0
+      ipv6_cidr_block = ""
+    }
+  ]
+
+  tags = {
+    Name = "acl-${var.test_prefix}-${var.test_suffix}"
+  }
+}
+
+
+resource "aws_eip" "aws_ec2_vpc_eip" {
+  vpc   = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_nat_gateway" "aws_ec2_vpc_nat_gateway" {
+  allocation_id = aws_eip.aws_ec2_vpc_eip.id
+//  connectivity_type = "private"
+  subnet_id         = aws_subnet.aws_vpc_subnet.id
+  tags = {
+    Name = "nat-${var.test_prefix}-${var.test_suffix}"
   }
 }
 
