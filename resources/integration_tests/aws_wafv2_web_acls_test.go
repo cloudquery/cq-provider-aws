bf8cc401515d30ecc03ba5b387e798310169d2f2
Timur Irmatov <irmatov@gmail.com>
2021-08-31 16:21:01 +0500

    extend a test

--- resources/integration_tests/aws_wafv2_web_acls_test.go
+++ resources/integration_tests/aws_wafv2_web_acls_test.go
@@ -33,6 +33,70 @@ func TestIntegrationWAFv2WebACLs(t *testing.T) {
 					},
 				},
 			}},
+			Relations: []*providertest.ResourceIntegrationVerification{
+				{
+					Name:           "aws_wafv2_web_acl_rules",
+					ForeignKeyName: "web_acl_cq_id",
+					ExpectedValues: []providertest.ExpectedValue{{
+						Count: 1,
+						Data: map[string]interface{}{
+							"name":     "rule-1",
+							"priority": float64(1),
+							"visibility_config_cloud_watch_metrics_enabled": false,
+							"visibility_config_metric_name":                 "friendly-rule-metric-name",
+							"visibility_config_sampled_requests_enabled":    false,
+							"override_action": map[string]interface{}{
+								"None": nil,
+								"Count": map[string]interface{}{
+									"CustomRequestHandling": nil,
+								},
+							},
+							"statement": map[string]interface{}{
+								"OrStatement":             nil,
+								"AndStatement":            nil,
+								"NotStatement":            nil,
+								"GeoMatchStatement":       nil,
+								"XssMatchStatement":       nil,
+								"ByteMatchStatement":      nil,
+								"RateBasedStatement":      nil,
+								"SqliMatchStatement":      nil,
+								"LabelMatchStatement":     nil,
+								"IPSetReferenceStatement": nil,
+								"SizeConstraintStatement": nil,
+								"ManagedRuleGroupStatement": map[string]interface{}{
+									"Name":       "AWSManagedRulesCommonRuleSet",
+									"VendorName": "AWS",
+									"ExcludedRules": []interface{}{
+										map[string]interface{}{"Name": "SizeRestrictions_QUERYSTRING"},
+										map[string]interface{}{"Name": "NoUserAgent_HEADER"},
+									},
+									"ScopeDownStatement": map[string]interface{}{
+										"OrStatement":  nil,
+										"AndStatement": nil,
+										"NotStatement": nil,
+										"GeoMatchStatement": map[string]interface{}{
+											"CountryCodes":      []interface{}{"US", "NL"},
+											"ForwardedIPConfig": nil,
+										},
+										"XssMatchStatement":                 nil,
+										"ByteMatchStatement":                nil,
+										"RateBasedStatement":                nil,
+										"SqliMatchStatement":                nil,
+										"LabelMatchStatement":               nil,
+										"IPSetReferenceStatement":           nil,
+										"SizeConstraintStatement":           nil,
+										"ManagedRuleGroupStatement":         nil,
+										"RuleGroupReferenceStatement":       nil,
+										"RegexPatternSetReferenceStatement": nil,
+									},
+								},
+								"RuleGroupReferenceStatement":       nil,
+								"RegexPatternSetReferenceStatement": nil,
+							},
+						},
+					}},
+				},
+			},
 		}
 	})
 }
