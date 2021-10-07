261a2f90aa15405d1036870d9bff75855fcfcb29
Andrii Romanenko <andrejjrom@gmail.com>
2021-10-07 10:27:01 +0300

    migrations fixes

--- resources/migrations/5_v0.5.14.down.sql
+++ resources/migrations/5_v0.5.14.down.sql
@@ -1,42 +1,42 @@
 --aws_apigatewayv2_vpc_links
-ALTER TABLE "aws_apigatewayv2_vpc_links"
+ALTER TABLE IF EXISTS "aws_apigatewayv2_vpc_links"
     ADD COLUMN "vpc_link_id" TEXT;
 
 UPDATE "aws_apigatewayv2_vpc_links"
 SET "vpc_link_id" = "id";
 
 --aws_cloudtrail_trails
-ALTER TABLE "aws_cloudtrail_trails"
+ALTER TABLE IF EXISTS "aws_cloudtrail_trails"
 DROP
 COLUMN "tags";
-ALTER TABLE "aws_cloudtrail_trails"
+ALTER TABLE IF EXISTS "aws_cloudtrail_trails"
     ADD COLUMN "home_region" TEXT;
 UPDATE "aws_cloudtrail_trails"
 SET "home_region" = "region";
 
 --aws_elasticbeanstalk_environments
-ALTER TABLE "aws_elasticbeanstalk_environments"
+ALTER TABLE IF EXISTS "aws_elasticbeanstalk_environments"
 DROP
 COLUMN "tags";
-ALTER TABLE "aws_elasticbeanstalk_environments" RENAME COLUMN "name" TO "environment_name";
+ALTER TABLE IF EXISTS "aws_elasticbeanstalk_environments" RENAME COLUMN "name" TO "environment_name";
 
 --aws_elasticsearch_domains
-ALTER TABLE "aws_elasticsearch_domains"
+ALTER TABLE IF EXISTS "aws_elasticsearch_domains"
 DROP
 COLUMN "tags";
 
 --aws_elbv2_load_balancers
-ALTER TABLE "aws_elbv2_load_balancers"
+ALTER TABLE IF EXISTS "aws_elbv2_load_balancers"
 DROP
 COLUMN "tags";
 
 --aws_elbv2_target_groups
-ALTER TABLE "aws_elbv2_target_groups"
+ALTER TABLE IF EXISTS "aws_elbv2_target_groups"
 DROP
 COLUMN "tags";
 
 --aws_kms_keys
-ALTER TABLE "aws_kms_keys"
+ALTER TABLE IF EXISTS "aws_kms_keys"
 DROP
 COLUMN "tags";
-ALTER TABLE "aws_kms_keys" RENAME COLUMN "id" TO "key_id";
+ALTER TABLE IF EXISTS "aws_kms_keys" RENAME COLUMN "id" TO "key_id";
--- resources/migrations/5_v0.5.14.up.sql
+++ resources/migrations/5_v0.5.14.up.sql
@@ -1,36 +1,38 @@
 --aws_apigatewayv2_vpc_links
-ALTER TABLE "aws_apigatewayv2_vpc_links"
+ALTER TABLE IF EXISTS "aws_apigatewayv2_vpc_links"
 DROP
 COLUMN "vpc_link_id";
 
 --aws_cloudtrail_trails
-ALTER TABLE "aws_cloudtrail_trails"
+ALTER TABLE IF EXISTS "aws_cloudtrail_trails"
     ADD COLUMN "tags" json;
-ALTER TABLE "aws_cloudtrail_trails"
+
+-- home_region duplicates region column so we removed it
+ALTER TABLE IF EXISTS "aws_cloudtrail_trails"
 DROP
 COLUMN "home_region";
 
 --aws_elasticbeanstalk_environments
-ALTER TABLE "aws_elasticbeanstalk_environments"
+ALTER TABLE IF EXISTS "aws_elasticbeanstalk_environments"
     ADD COLUMN "tags" json;
-ALTER TABLE "aws_elasticbeanstalk_environments" RENAME COLUMN "environment_name" TO "name";
+ALTER TABLE IF EXISTS "aws_elasticbeanstalk_environments" RENAME COLUMN "environment_name" TO "name";
 
 --aws_elasticsearch_domains
-ALTER TABLE "aws_elasticsearch_domains"
+ALTER TABLE IF EXISTS "aws_elasticsearch_domains"
     ADD COLUMN "tags" json;
 
 --aws_elbv2_load_balancers
-ALTER TABLE "aws_elbv2_load_balancers"
+ALTER TABLE IF EXISTS "aws_elbv2_load_balancers"
     ADD COLUMN "tags" json;
 
 --aws_elbv2_target_groups
-ALTER TABLE "aws_elbv2_target_groups"
+ALTER TABLE IF EXISTS "aws_elbv2_target_groups"
     ADD COLUMN "tags" json;
 
 --aws_kms_keys
-ALTER TABLE "aws_kms_keys"
+ALTER TABLE IF EXISTS "aws_kms_keys"
     ADD COLUMN "tags" json;
-ALTER TABLE "aws_kms_keys" RENAME COLUMN "key_id" TO "id";
+ALTER TABLE IF EXISTS "aws_kms_keys" RENAME COLUMN "key_id" TO "id";
 
 
 
