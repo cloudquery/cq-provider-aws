8047074a3e30573cc1ed5be288a6218a91c80896
bbernays <ben@bernays.me>
2021-11-24 06:24:13 -0500

    Make pass tests

--- docs/docs.go
+++ docs/docs.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
 
 	"github.com/cloudquery/cq-provider-aws/resources"
 	"github.com/cloudquery/cq-provider-sdk/provider/docs"
@@ -10,10 +12,18 @@ import (
 
 func main() {
 	outputPath := "./docs"
-	err := os.RemoveAll(outputPath)
+	dir, err := ioutil.ReadDir(outputPath + "/tables")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to generate docs: %s\n", err)
 		os.Exit(1)
+	}
+	for _, d := range dir {
+		err = os.RemoveAll(path.Join([]string{outputPath + "/tables", d.Name()}...))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to generate docs: %s\n", err)
+			os.Exit(1)
+		}
+
 	}
 
 	err = docs.GenerateDocs(resources.Provider(), outputPath)
