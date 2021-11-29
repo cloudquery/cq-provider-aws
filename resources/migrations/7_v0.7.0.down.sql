cd7b53499ee77f3157bd5c26d2e90d10210aa772
Itay Zagron <zagron@gmail.com>
2021-11-29 18:21:32 +0100

    remove conflicts

--- resources/migrations/7_v0.7.0.down.sql
+++ resources/migrations/7_v0.7.0.down.sql
@@ -1,6 +1,5 @@
 ALTER TABLE IF EXISTS "aws_iam_password_policies" DROP COLUMN policy_exists;
 
-<<<<<<< Updated upstream:resources/migrations/7_v0.6.5.down.sql
 ALTER TABLE IF EXISTS "aws_directconnect_gateways" RENAME COLUMN "state" TO "direct_connect_gateway_state";
 
 ALTER TABLE IF EXISTS "aws_directconnect_gateways" RENAME COLUMN "name" TO "direct_connect_gateway_name";
@@ -16,6 +15,5 @@ ALTER TABLE IF EXISTS "aws_directconnect_gateway_associations" RENAME COLUMN "ga
 ALTER TABLE IF EXISTS "aws_directconnect_gateway_attachments" DROP COLUMN "gateway_id";
 
 ALTER TABLE IF EXISTS "aws_elbv2_listeners" DROP COLUMN "load_balancer_cq_id";
-=======
+
 ALTER TABLE IF EXISTS "aws_sns_topics" DROP COLUMN kms_master_key_id;
->>>>>>> Stashed changes:resources/migrations/7_v0.7.0.down.sql
--- resources/migrations/7_v0.7.0.up.sql
+++ resources/migrations/7_v0.7.0.up.sql
@@ -1,7 +1,5 @@
 ALTER TABLE IF EXISTS "aws_iam_password_policies" ADD COLUMN policy_exists boolean;
 
-<<<<<<< Updated upstream:resources/migrations/7_v0.6.5.up.sql
-
 ALTER TABLE IF EXISTS "aws_directconnect_gateways" RENAME COLUMN "direct_connect_gateway_state" TO "state";
 
 ALTER TABLE IF EXISTS "aws_directconnect_gateways" RENAME COLUMN "direct_connect_gateway_name" TO "name";
@@ -14,8 +12,6 @@ ALTER TABLE IF EXISTS "aws_directconnect_gateway_associations" RENAME COLUMN "di
 
 ALTER TABLE IF EXISTS "aws_directconnect_gateway_attachments" ADD COLUMN "gateway_id" text;
 
-
 ALTER TABLE IF EXISTS "aws_elbv2_listeners" ADD COLUMN "load_balancer_cq_id" uuid;
-=======
+
 ALTER TABLE IF EXISTS "aws_sns_topics" ADD COLUMN kms_master_key_id text;
->>>>>>> Stashed changes:resources/migrations/7_v0.7.0.up.sql
