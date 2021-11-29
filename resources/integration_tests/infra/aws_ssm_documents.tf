e69b8c4e2a5bf02d0f9fad3076adf238528f4584
Timur Irmatov <irmatov@gmail.com>
2021-11-29 16:00:09 +0500

    Implement SSM documents

---
+++ resources/integration_tests/infra/aws_ssm_documents.tf
@@ -0,0 +1,29 @@
+resource "aws_ssm_document" "mytestdoc" {
+  name          = "${var.test_prefix}doc${var.test_suffix}"
+  document_type = "Command"
+
+  content = <<DOC
+  {
+    "schemaVersion": "1.2",
+    "description": "Check ip configuration of a Linux instance.",
+    "parameters": {
+
+    },
+    "runtimeConfig": {
+      "aws:runShellScript": {
+        "properties": [
+          {
+            "id": "0.aws:runShellScript",
+            "runCommand": ["ifconfig"]
+          }
+        ]
+      }
+    }
+  }
+DOC
+
+  permissions = {
+    type = "Share"
+    account_ids = "All"
+  }
+}
