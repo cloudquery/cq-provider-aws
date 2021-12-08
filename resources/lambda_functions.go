eb21a8123ab1a0347eca4523a96438d39eeceef5
Itay Zagron <zagron@gmail.com>
2021-12-08 09:31:37 +0100

    extract to standalone resource

--- resources/lambda_functions.go
+++ resources/lambda_functions.go
@@ -916,19 +916,6 @@ func LambdaFunctions() *schema.Table {
 					},
 				},
 			},
-			{
-				Name:        "aws_lambda_runtimes",
-				Description: "All known values for Runtime",
-				Resolver:    fetchLambdaRuntimes,
-				Options:     schema.TableCreationOptions{PrimaryKeys: []string{"name"}},
-				Columns: []schema.Column{
-					{
-						Name:        "name",
-						Description: "Runtime name",
-						Type:        schema.TypeString,
-					},
-				},
-			},
 		},
 	}
 }
@@ -1221,28 +1208,4 @@ func fetchLambdaFunctionEventSourceMappings(ctx context.Context, meta schema.Cli
 		config.Marker = output.NextMarker
 	}
 	return nil
-}
-func fetchLambdaRuntimes(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan interface{}) error {
-	for _, runtime := range types.RuntimeProvidedal2.Values() {
-		res <- &RuntimeWrapper{
-			Name: string(runtime),
-		}
-	}
-
-	return nil
-}
-func resolveLambdaFunctionEventSourceMappingAccessConfigurations(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, c schema.Column) error {
-	p, ok := resource.Item.(types.EventSourceMappingConfiguration)
-	if !ok {
-		return fmt.Errorf("wrong type assertion: got %T instead of EventSourceMappingConfiguration", p)
-	}
-	data, err := json.Marshal(p.SourceAccessConfigurations)
-	if err != nil {
-		return err
-	}
-	return resource.Set(c.Name, data)
-}
-
-type RuntimeWrapper struct {
-	Name string
 }
