e941b17a2657924fdc5442ed2b05ba8dfb3815e2
Kemal Hadimli <disq@users.noreply.github.com>
2021-12-17 19:36:23 +0000

    Remove the test as it'll go in the SDK

--- resources/provider_test.go
+++ resources/provider_test.go
@@ -9,7 +9,6 @@ import (
 	providertest "github.com/cloudquery/cq-provider-sdk/provider/testing"
 	"github.com/golang/mock/gomock"
 	"github.com/hashicorp/go-hclog"
-	"github.com/stretchr/testify/assert"
 )
 
 type TestOptions struct {
@@ -38,33 +37,4 @@ func awsTestHelper(t *testing.T, table *schema.Table, builder func(*testing.T, *
 		},
 		SkipEmptyJsonB: options.SkipEmptyJsonB,
 	})
-}
-
-func TestTableIdentifiers(t *testing.T) {
-	t.Parallel()
-	for _, res := range Provider().ResourceMap {
-		res := res
-		t.Run(res.Name, func(t *testing.T) {
-			testTableIdentifiers(t, res)
-		})
-	}
-}
-
-func testTableIdentifiers(t *testing.T, table *schema.Table) {
-	const maxIdentifierLength = 63 // maximum allowed identifier length is 63 bytes https://www.postgresql.org/docs/13/limits.html
-
-	assert.NotEmpty(t, table.Name)
-	assert.LessOrEqual(t, len(table.Name), maxIdentifierLength, "Table name too long")
-
-	for _, c := range table.Columns {
-		assert.NotEmpty(t, c.Name)
-		assert.LessOrEqual(t, len(c.Name), maxIdentifierLength, "Column name too long:", c.Name)
-	}
-
-	for _, res := range table.Relations {
-		res := res
-		t.Run(res.Name, func(t *testing.T) {
-			testTableIdentifiers(t, res)
-		})
-	}
 }
