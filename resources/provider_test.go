954d4c923d6d5a4c40b395317e669548a5273d33
Kemal Hadimli <disq@users.noreply.github.com>
2021-12-23 14:26:28 +0000

    Add test: MultiplexerPKMatch

--- resources/provider_test.go
+++ resources/provider_test.go
@@ -9,6 +9,7 @@ import (
 	providertest "github.com/cloudquery/cq-provider-sdk/provider/testing"
 	"github.com/golang/mock/gomock"
 	"github.com/hashicorp/go-hclog"
+	"github.com/stretchr/testify/assert"
 )
 
 type TestOptions struct {
@@ -37,4 +38,50 @@ func awsTestHelper(t *testing.T, table *schema.Table, builder func(*testing.T, *
 		},
 		SkipEmptyJsonB: options.SkipEmptyJsonB,
 	})
+}
+
+func TestMultiplexerPKMatch(t *testing.T) {
+	for res, table := range Provider().ResourceMap {
+		if table.Multiplex == nil {
+			continue
+		}
+
+		t.Run(res, func(t *testing.T) {
+			cfgRegions := []string{"us-east-1", "eu-west-1"}
+
+			c := client.NewAwsClient(logging.New(&hclog.LoggerOptions{
+				Level: hclog.Warn,
+			}), []client.Account{{ID: "test1"}, {ID: "test2"}}, cfgRegions)
+			for _, a := range c.Accounts {
+				for _, r := range cfgRegions {
+					c.ServicesManager.InitServicesForAccountAndRegion(a.AccountID, r, client.Services{})
+				}
+			}
+
+			multiClients := table.Multiplex(schema.ClientMeta(&c))
+
+			regions, accounts := make(map[string]struct{}), make(map[string]struct{})
+			for _, c := range multiClients {
+				cc := c.(*client.Client)
+				accounts[cc.AccountID], regions[cc.Region] = struct{}{}, struct{}{}
+			}
+			requiredPKs := make(map[string]struct{})
+			if len(accounts) > 1 {
+				requiredPKs["account_id"] = struct{}{}
+			}
+			if len(regions) > 1 {
+				requiredPKs["region"] = struct{}{}
+			}
+			if len(requiredPKs) == 0 {
+				return
+			}
+			// Multiple accounts OR regions confirmed
+
+			for _, c := range table.PrimaryKeys() {
+				delete(requiredPKs, c)
+			}
+
+			assert.Zero(t, len(requiredPKs), "Missing PKs: %+v", requiredPKs)
+		})
+	}
 }
