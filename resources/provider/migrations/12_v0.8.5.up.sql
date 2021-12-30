4a4e1f7a2365e8887da062326c26d9d27130c7d9
Kemal Hadimli <disq@users.noreply.github.com>
2021-12-30 17:39:23 +0000

    Fix PK order

--- resources/provider/migrations/12_v0.8.5.up.sql
+++ resources/provider/migrations/12_v0.8.5.up.sql
@@ -2,4 +2,4 @@ ALTER TABLE IF EXISTS "aws_elasticsearch_domains" DROP CONSTRAINT IF EXISTS "aws
 
 ALTER TABLE "aws_elasticsearch_domains"
     ADD CONSTRAINT "aws_elasticsearch_domains_pk"
-        PRIMARY KEY(account_id, id, region);
+        PRIMARY KEY(account_id, region, id);
