dd47923783924fa0154c9c6ff791f3de00ba020b
Kemal Hadimli <disq@users.noreply.github.com>
2022-01-07 10:53:59 +0000

    Call the cli

--- tools/migrations/main.go
+++ tools/migrations/main.go
@@ -7,13 +7,11 @@ import (
 
 	"github.com/cloudquery/cq-provider-aws/resources/provider"
 	"github.com/cloudquery/cq-provider-sdk/provider/migrations"
-	"github.com/hashicorp/go-hclog"
 )
 
 func main() {
-	const outputPath = "./resources/provider/migrations"
-
-	if err := migrations.Generate(context.Background(), hclog.L(), provider.Provider(), outputPath, ""); err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to generate migrations: %s\n", err)
+	if err := migrations.Run(context.Background(), provider.Provider(), ""); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
+		os.Exit(1)
 	}
 }
