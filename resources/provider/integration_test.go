57aa5db39599b6aefa70fbd2cd493183f0b345c7
Yevgeny Pats <yev.pats@gmail.com>
2022-01-30 14:58:11 +0200

    test: Add CQ_TEST_CFG to override config in tests

--- resources/provider/integration_test.go
+++ resources/provider/integration_test.go
@@ -3,23 +3,31 @@
 package provider
 
 import (
+	"os"
 	"testing"
 
 	providertest "github.com/cloudquery/cq-provider-sdk/provider/testing"
 )
 
 func TestIntegration(t *testing.T) {
-
 	cfg := `
-	accounts "cq-provider-aws" {
-		role_arn = "arn:aws:iam::707066037989:role/CqProviderAWSAssumeRoleTest"
+		max_retries = 20
+  		max_backoff = 60
+  	`
+	// we want to give a way for the user ci to override with additional cfg for integration test.
+	// For example:
+	//
+	// accounts "cq-provider-aws" {
+	// 	role_arn = "arn:aws:iam::70xxxxxxxxxx:role/CqProviderxxxxxxxxxxxxxxxxx"
+	// }
+	// accounts "cq-dev" {
+	//  role_arn = "arn:aws:iam::70xxxxxxxxxx:role/CqProviderxxxxxxxxxxxxxxxxx"
+	// }
+
+	additionalConfig := os.Getenv("CQ_TEST_CFG")
+	if additionalConfig != "" {
+		cfg += additionalConfig
 	}
-	accounts "cq-dev" {
-		role_arn = "arn:aws:iam::704956590351:role/CqProviderAWSAssumeRoleTest"
-	}
-  	max_retries = 20
-	max_backoff = 60
-	`
 
 	providertest.TestResource(t, providertest.ResourceTestCase{
 		Provider:              Provider(),
