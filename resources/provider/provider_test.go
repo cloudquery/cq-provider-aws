c602e518718c0dd8813fdc672774bf96e9b5fded
Yevgeny Pats <yev.pats@gmail.com>
2022-02-03 12:43:50 +0200

    test: Merge integration tests with unittests

--- resources/provider/provider_test.go
+++ resources/provider/provider_test.go
@@ -1,12 +1,30 @@
+// go:build migrations
 package provider_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/cloudquery/cq-provider-aws/resources/provider"
 	"github.com/cloudquery/cq-provider-sdk/migration"
 )
 
-func TestMigrations(t *testing.T) {
+func TestMigrationPostgreSQL(t *testing.T) {
+	dsn := os.Getenv("CQ_MIGRATION_TEST_PG_DSN")
+	if dsn == "" {
+		os.Setenv("CQ_MIGRATION_TEST_DSN", "postgres://postgres:pass@localhost:5432/postgres?sslmode=disable")
+	} else {
+		os.Setenv("CQ_MIGRATION_TEST_DSN", dsn)
+	}
+	migration.RunMigrationsTest(t, provider.Provider(), []string{"latest"})
+}
+
+func TestMigrationTimescaleDB(t *testing.T) {
+	dsn := os.Getenv("CQ_MIGRATION_TEST_TSDB_DSN")
+	if dsn == "" {
+		os.Setenv("CQ_MIGRATION_TEST_DSN", "tsdb://postgres:pass@localhost:5432/postgres?sslmode=disable")
+	} else {
+		os.Setenv("CQ_MIGRATION_TEST_DSN", dsn)
+	}
 	migration.RunMigrationsTest(t, provider.Provider(), []string{"latest"})
 }
