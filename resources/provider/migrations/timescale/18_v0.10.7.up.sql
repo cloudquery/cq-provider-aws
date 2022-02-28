4cc80cd2aafa797d168e660f11c212b042537a3c
Kemal Hadimli <disq@users.noreply.github.com>
2022-02-28 09:39:05 +0000

    fix PK

--- resources/provider/migrations/timescale/18_v0.10.7.up.sql
+++ resources/provider/migrations/timescale/18_v0.10.7.up.sql
@@ -1,4 +1,4 @@
 ALTER TABLE IF EXISTS aws_route53_traffic_policy_versions
     DROP CONSTRAINT aws_route53_traffic_policy_versions_pk;
 ALTER TABLE IF EXISTS aws_route53_traffic_policy_versions
-    ADD CONSTRAINT aws_route53_traffic_policy_versions_pk PRIMARY KEY (traffic_policy_cq_id, id, version);
+    ADD CONSTRAINT aws_route53_traffic_policy_versions_pk PRIMARY KEY (cq_fetch_date, traffic_policy_cq_id, id, version);
