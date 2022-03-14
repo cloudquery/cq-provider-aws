883c8864f9df8427beeca70897d56bc0f203bb3b
Andrii Romanenko <andrejjrom@gmail.com>
2022-03-14 17:54:59 +0200

    added migrations

--- resources/provider/migrations/timescale/21_v0.10.12.down.sql
+++ resources/provider/migrations/timescale/21_v0.10.12.down.sql
@@ -4,3 +4,5 @@ ALTER TABLE IF EXISTS aws_ec2_eips
     DROP CONSTRAINT aws_ec2_eips_pk;
 ALTER TABLE IF EXISTS aws_ec2_eips
     ADD CONSTRAINT aws_ec2_eips_pk PRIMARY KEY (cq_fetch_date,account_id,public_ip);
+
+DROP TABLE IF EXISTS aws_access_analyzer_analyzer_archive_rules;
--- resources/provider/migrations/timescale/21_v0.10.12.up.sql
+++ resources/provider/migrations/timescale/21_v0.10.12.up.sql
@@ -5,3 +5,19 @@ ALTER TABLE IF EXISTS aws_ec2_eips
 ALTER TABLE IF EXISTS aws_ec2_eips
     ADD CONSTRAINT aws_ec2_eips_pk PRIMARY KEY (cq_fetch_date,account_id,allocation_id);
 
+CREATE TABLE IF NOT EXISTS "aws_access_analyzer_analyzer_archive_rules" (
+                                                                            "cq_id" uuid NOT NULL,
+                                                                            "cq_meta" jsonb,
+                                                                            "cq_fetch_date" timestamp without time zone NOT NULL,
+                                                                            "analyzer_cq_id" uuid,
+                                                                            "created_at" timestamp without time zone,
+                                                                            "filter" jsonb,
+                                                                            "rule_name" text,
+                                                                            "updated_at" timestamp without time zone,
+                                                                            CONSTRAINT aws_access_analyzer_analyzer_archive_rules_pk PRIMARY KEY(cq_fetch_date,cq_id),
+                                                                            UNIQUE(cq_fetch_date,cq_id)
+);
+CREATE INDEX ON aws_access_analyzer_analyzer_archive_rules (cq_fetch_date, analyzer_cq_id);
+SELECT setup_tsdb_child('aws_access_analyzer_analyzer_archive_rules', 'analyzer_cq_id', 'aws_access_analyzer_analyzers', 'cq_id');
+
+
