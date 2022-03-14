e5fea77384c39b037568d87265eaa259e2d864ba
Andrii Romanenko <andrejjrom@gmail.com>
2022-03-14 18:27:02 +0200

    linter

--- resources/provider/migrations/postgres/21_v0.10.12.down.sql
+++ resources/provider/migrations/postgres/21_v0.10.12.down.sql
@@ -3,6 +3,6 @@
 ALTER TABLE IF EXISTS aws_ec2_eips
     DROP CONSTRAINT aws_ec2_eips_pk;
 ALTER TABLE IF EXISTS aws_ec2_eips
-    ADD CONSTRAINT aws_ec2_eips_pk PRIMARY KEY (account_id,public_ip);
+    ADD CONSTRAINT aws_ec2_eips_pk PRIMARY KEY (account_id, public_ip);
 
 DROP TABLE IF EXISTS aws_access_analyzer_analyzer_archive_rules;
