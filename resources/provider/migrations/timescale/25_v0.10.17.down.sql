5c00d8c06e0f648ea5dfe77c75efc9cbe9e49997
Kemal Hadimli <disq@users.noreply.github.com>
2022-04-12 17:07:32 +0100

    foiled

--- resources/provider/migrations/timescale/25_v0.10.17.down.sql
+++ resources/provider/migrations/timescale/25_v0.10.17.down.sql
@@ -6,4 +6,4 @@ DROP TABLE IF EXISTS aws_wafv2_regex_pattern_sets;
 
 -- Resource: iam.virtual_mfa_devices
 ALTER TABLE IF EXISTS aws_iam_virtual_mfa_devices DROP CONSTRAINT aws_iam_virtual_mfa_devices_pk;
-ALTER TABLE IF EXISTS aws_iam_virtual_mfa_devices ADD CONSTRAINT aws_iam_virtual_mfa_devices_pk PRIMARY KEY (serial_number,enable_date);
+ALTER TABLE IF EXISTS aws_iam_virtual_mfa_devices ADD CONSTRAINT aws_iam_virtual_mfa_devices_pk PRIMARY KEY (cq_fetch_date,serial_number,enable_date);
--- resources/provider/migrations/timescale/25_v0.10.17.up.sql
+++ resources/provider/migrations/timescale/25_v0.10.17.up.sql
@@ -39,4 +39,4 @@ SELECT setup_tsdb_parent('aws_wafv2_regex_pattern_sets');
 
 -- Resource: iam.virtual_mfa_devices
 ALTER TABLE IF EXISTS aws_iam_virtual_mfa_devices DROP CONSTRAINT aws_iam_virtual_mfa_devices_pk;
-ALTER TABLE IF EXISTS aws_iam_virtual_mfa_devices ADD CONSTRAINT aws_iam_virtual_mfa_devices_pk PRIMARY KEY (serial_number);
+ALTER TABLE IF EXISTS aws_iam_virtual_mfa_devices ADD CONSTRAINT aws_iam_virtual_mfa_devices_pk PRIMARY KEY (cq_fetch_date,serial_number);
