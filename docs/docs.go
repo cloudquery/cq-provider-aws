add62309ed756f808d20705155ccbacbb38fa9c1
Daniel Spangenberg <daniel@cloudquery.io>
2022-04-22 18:03:29 +0200

    chore: Trigger CI for cache validation

--- docs/docs.go
+++ docs/docs.go
@@ -5,12 +5,13 @@ import (
 	"os"
 
 	"github.com/cloudquery/cq-provider-aws/resources/provider"
+
 	"github.com/cloudquery/cq-provider-sdk/provider/docs"
 )
 
 func main() {
 	outputPath := "./docs"
 	if err := docs.GenerateDocs(provider.Provider(), outputPath, true); err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to generate docs: %s\n", err)
+		_, _ = fmt.Fprintf(os.Stderr, "Failed to generate docs: %s\n", err)
 	}
 }
