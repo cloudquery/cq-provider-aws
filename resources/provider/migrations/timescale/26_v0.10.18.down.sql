898325a4214443ec344a27fc97f0fd5e6cf5baeb
Andrii Romanenko <andrejjrom@gmail.com>
2022-04-26 14:44:52 +0300

    migrations fix

--- resources/provider/migrations/timescale/26_v0.10.18.down.sql
+++ resources/provider/migrations/timescale/26_v0.10.18.down.sql
@@ -36,16 +36,3 @@ DROP TABLE IF EXISTS aws_backup_global_settings;
 -- Resource: backup.region_settings
 DROP TABLE IF EXISTS aws_backup_region_settings;
 
--- Resource: shield.attacks
-DROP TABLE IF EXISTS aws_shield_attack_properties;
-DROP TABLE IF EXISTS aws_shield_attack_sub_resources;
-DROP TABLE IF EXISTS aws_shield_attacks;
-
--- Resource: shield.protections
-DROP TABLE IF EXISTS aws_shield_protections;
-
--- Resource: shield.protections_groups
-DROP TABLE IF EXISTS aws_shield_protection_groups;
-
--- Resource: shield.subscriptions
-DROP TABLE IF EXISTS aws_shield_subscriptions;
