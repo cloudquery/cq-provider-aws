0001d10b84d03c9115bfc38d467312bead5374a1
Andrii Romanenko <andrejjrom@gmail.com>
2022-04-27 21:59:44 +0300

    added migration

---
+++ resources/provider/migrations/postgres/27_v0.10.19.down.sql
@@ -0,0 +1,6 @@
+-- Resource: athena.data_catalogs
+DROP TABLE IF EXISTS aws_athena_data_catalog_database_table_columns;
+DROP TABLE IF EXISTS aws_athena_data_catalog_database_table_partition_keys;
+DROP TABLE IF EXISTS aws_athena_data_catalog_database_tables;
+DROP TABLE IF EXISTS aws_athena_data_catalog_databases;
+DROP TABLE IF EXISTS aws_athena_data_catalogs;
