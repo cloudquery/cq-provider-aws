b1614f3fbcd320e172298e7f8503aa0fe99770c6
Itay Zagron <zagron@gmail.com>
2022-05-02 11:25:12 +0300

    fixed migration

--- resources/provider/migrations/timescale/28_v0.11.2.down.sql
+++ resources/provider/migrations/timescale/28_v0.11.2.down.sql
@@ -4,8 +4,8 @@ ALTER TABLE IF EXISTS "aws_sns_topics" DROP COLUMN IF EXISTS "tags";
 
 -- Resource: waf.web_acls
 DROP TABLE IF EXISTS aws_waf_web_acl_logging_configuration;
-ALTER TABLE IF EXISTS "aws_waf_web_acls" ADD COLUMN IF NOT EXISTS "logging_configuration" array;
+ALTER TABLE IF EXISTS "aws_waf_web_acls" ADD COLUMN IF NOT EXISTS "logging_configuration" TEXT[];
 
 -- Resource: wafv2.web_acls
 DROP TABLE IF EXISTS aws_wafv2_web_acl_logging_configuration;
-ALTER TABLE IF EXISTS "aws_wafv2_web_acls" ADD COLUMN IF NOT EXISTS "logging_configuration" array;
+ALTER TABLE IF EXISTS "aws_wafv2_web_acls" ADD COLUMN IF NOT EXISTS "logging_configuration" TEXT[];
