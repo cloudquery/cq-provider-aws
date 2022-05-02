a35860b391fc4978242efb9015f3d0038350a54b
Andrii Romanenko <andrejjrom@gmail.com>
2022-05-02 17:59:51 +0300

    migration adjusted

--- resources/provider/migrations/postgres/27_v0.11.1.down.sql
+++ resources/provider/migrations/postgres/27_v0.11.1.down.sql
@@ -11,3 +11,10 @@ DROP TABLE IF EXISTS aws_athena_data_catalog_database_tables;
 DROP TABLE IF EXISTS aws_athena_data_catalog_databases;
 DROP TABLE IF EXISTS aws_athena_data_catalogs;
 
+-- Resource: athena.work_groups
+DROP TABLE IF EXISTS aws_athena_work_group_prepared_statements;
+DROP TABLE IF EXISTS aws_athena_work_group_query_executions;
+DROP TABLE IF EXISTS aws_athena_work_group_named_queries;
+DROP TABLE IF EXISTS aws_athena_work_groups;
+
+
