5ebbc46fd6e06ccf547ca36a9a9583f993e3e9b8
bbernays <ben@bernays.me>
2022-05-04 16:38:58 -0400

    Update 29_v0.11.3.up.sql

--- resources/provider/migrations/timescale/29_v0.11.3.up.sql
+++ resources/provider/migrations/timescale/29_v0.11.3.up.sql
@@ -2,15 +2,15 @@
 -- CHANGEME: Verify or edit this file before proceeding
 
 -- Resource: wafv2.managed_rule_groups
-TRUNCATE TABLE IF EXISTS aws_wafv2_managed_rule_groups;
+TRUNCATE TABLE aws_wafv2_managed_rule_groups;
 ALTER TABLE IF EXISTS aws_wafv2_managed_rule_groups DROP CONSTRAINT aws_wafv2_managed_rule_groups_pk;
 ALTER TABLE IF EXISTS "aws_wafv2_managed_rule_groups" ADD COLUMN IF NOT EXISTS "scope" text;
 ALTER TABLE IF EXISTS aws_wafv2_managed_rule_groups ADD CONSTRAINT aws_wafv2_managed_rule_groups_pk PRIMARY KEY (cq_fetch_date,account_id,region,scope,vendor_name,name);
 
 -- Resource: wafv2.rule_groups
-TRUNCATE TABLE IF EXISTS aws_wafv2_rule_groups;
+TRUNCATE TABLE aws_wafv2_rule_groups;
 ALTER TABLE IF EXISTS "aws_wafv2_rule_groups" ADD COLUMN IF NOT EXISTS "scope" text;
 
 -- Resource: wafv2.web_acls
-TRUNCATE TABLE IF EXISTS aws_wafv2_web_acls;
+TRUNCATE TABLE aws_wafv2_web_acls;
 ALTER TABLE IF EXISTS "aws_wafv2_web_acls" ADD COLUMN IF NOT EXISTS "scope" text;
