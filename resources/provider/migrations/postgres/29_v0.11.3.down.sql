660146d6136a73e1141d3762e0656e5fe263e864
Andrii Romanenko <andrejjrom@gmail.com>
2022-05-10 12:40:28 +0300

    migrations fix

--- resources/provider/migrations/postgres/29_v0.11.3.down.sql
+++ resources/provider/migrations/postgres/29_v0.11.3.down.sql
@@ -37,16 +37,3 @@ ALTER TABLE IF EXISTS "aws_wafv2_rule_groups" DROP COLUMN IF EXISTS "scope";
 -- Resource: wafv2.web_acls
 TRUNCATE TABLE aws_wafv2_web_acls CASCADE;
 ALTER TABLE IF EXISTS "aws_wafv2_web_acls" DROP COLUMN IF EXISTS "scope";
-
--- Resource: athena.data_catalogs
-DROP TABLE IF EXISTS aws_athena_data_catalog_database_table_columns;
-DROP TABLE IF EXISTS aws_athena_data_catalog_database_table_partition_keys;
-DROP TABLE IF EXISTS aws_athena_data_catalog_database_tables;
-DROP TABLE IF EXISTS aws_athena_data_catalog_databases;
-DROP TABLE IF EXISTS aws_athena_data_catalogs;
-
--- Resource: athena.work_groups
-DROP TABLE IF EXISTS aws_athena_work_group_prepared_statements;
-DROP TABLE IF EXISTS aws_athena_work_group_query_executions;
-DROP TABLE IF EXISTS aws_athena_work_group_named_queries;
-DROP TABLE IF EXISTS aws_athena_work_groups;
