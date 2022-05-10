356ac269c305a2f2d68e01bb61b149c65bead4a8
Andrii Romanenko <andrejjrom@gmail.com>
2022-05-10 12:42:17 +0300

    migrations fix

--- resources/provider/migrations/postgres/30_v0.11.5.down.sql
+++ resources/provider/migrations/postgres/30_v0.11.5.down.sql
@@ -1,3 +1,17 @@
+-- Resource: shield.attacks
+DROP TABLE IF EXISTS aws_shield_attack_properties;
+DROP TABLE IF EXISTS aws_shield_attack_sub_resources;
+DROP TABLE IF EXISTS aws_shield_attacks;
+
+-- Resource: shield.protections
+DROP TABLE IF EXISTS aws_shield_protections;
+
+-- Resource: shield.protections_groups
+DROP TABLE IF EXISTS aws_shield_protection_groups;
+
+-- Resource: shield.subscriptions
+DROP TABLE IF EXISTS aws_shield_subscriptions;
+
 -- Resource: athena.data_catalogs
 DROP TABLE IF EXISTS aws_athena_data_catalog_database_table_columns;
 DROP TABLE IF EXISTS aws_athena_data_catalog_database_table_partition_keys;
