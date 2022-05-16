36576585f45ec7e59fe4713377131e1d7de84db8
Itay Zagron <zagron@gmail.com>
2022-05-16 11:00:36 +0300

    fixed tests

--- resources/provider/migrations/postgres/31_v0.11.6.up.sql
+++ resources/provider/migrations/postgres/31_v0.11.6.up.sql
@@ -157,22 +157,22 @@ ALTER TABLE IF EXISTS aws_sqs_queues ALTER COLUMN last_modified_timestamp TYPE t
 
 -- Resource: ec2.images
 TRUNCATE TABLE aws_ec2_images CASCADE;
-ALTER TABLE IF EXISTS aws_ec2_images ALTER COLUMN creation_date TYPE timestamp USING to_timestamp(creation_date, 'YYYY-MM-DDTHH:MM:SS.MS');
+ALTER TABLE IF EXISTS aws_ec2_images ALTER COLUMN creation_date TYPE timestamp USING to_timestamp(creation_date, 'YYYY-MM-DDTHH:MI:SS.MS');
 
 -- Resource: ec2.instances
 TRUNCATE TABLE aws_ec2_instance_elastic_gpu_associations CASCADE;
-ALTER TABLE IF EXISTS aws_ec2_instance_elastic_gpu_associations ALTER COLUMN elastic_gpu_association_time TYPE timestamp USING to_timestamp(elastic_gpu_association_time, 'YYYY-MM-DDTHH:MM:SS.MS');
+ALTER TABLE IF EXISTS aws_ec2_instance_elastic_gpu_associations ALTER COLUMN elastic_gpu_association_time TYPE timestamp USING to_timestamp(elastic_gpu_association_time, 'YYYY-MM-DDTHH:MI:SS.MS');
 
 -- Resource: lambda.layers
 TRUNCATE TABLE aws_lambda_layers CASCADE;
-ALTER TABLE IF EXISTS aws_lambda_layers ALTER COLUMN latest_matching_version_created_date TYPE timestamp USING to_timestamp(latest_matching_version_created_date, 'YYYY-MM-DDTHH:MM:SS.MSZ');
+ALTER TABLE IF EXISTS aws_lambda_layers ALTER COLUMN latest_matching_version_created_date TYPE timestamp USING to_timestamp(latest_matching_version_created_date, 'YYYY-MM-DDTHH:MI:SS.MS');
 TRUNCATE TABLE aws_lambda_layer_versions CASCADE;
-ALTER TABLE IF EXISTS aws_lambda_layer_versions ALTER COLUMN created_date TYPE timestamp USING to_timestamp(created_date, 'YYYY-MM-DDTHH:MM:SS.MSZ');
+ALTER TABLE IF EXISTS aws_lambda_layer_versions ALTER COLUMN created_date TYPE timestamp USING to_timestamp(created_date, 'YYYY-MM-DDTHH:MI:SS.MS');
 
 -- Resource: lambda.functions
 TRUNCATE TABLE aws_lambda_functions CASCADE;
-ALTER TABLE IF EXISTS aws_lambda_functions ALTER COLUMN last_modified TYPE timestamp USING to_timestamp(last_modified, 'YYYY-MM-DDTHH:MM:SS.MSZ');
+ALTER TABLE IF EXISTS aws_lambda_functions ALTER COLUMN last_modified TYPE timestamp USING to_timestamp(last_modified, 'YYYY-MM-DDTHH:MI:SS.MS');
 TRUNCATE TABLE aws_lambda_function_versions CASCADE;
-ALTER TABLE IF EXISTS aws_lambda_function_versions ALTER COLUMN last_modified TYPE timestamp USING to_timestamp(last_modified, 'YYYY-MM-DDTHH:MM:SS.MSZ');
+ALTER TABLE IF EXISTS aws_lambda_function_versions ALTER COLUMN last_modified TYPE timestamp USING to_timestamp(last_modified, 'YYYY-MM-DDTHH:MI:SS.MS');
 TRUNCATE TABLE aws_lambda_function_concurrency_configs CASCADE;
-ALTER TABLE IF EXISTS aws_lambda_function_concurrency_configs ALTER COLUMN last_modified TYPE timestamp USING to_timestamp(last_modified, 'YYYY-MM-DDTHH:MM:SS.MSZ');
+ALTER TABLE IF EXISTS aws_lambda_function_concurrency_configs ALTER COLUMN last_modified TYPE timestamp USING to_timestamp(last_modified, 'YYYY-MM-DDTHH:MI:SS.MS');
