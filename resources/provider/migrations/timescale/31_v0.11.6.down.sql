a4b14b4aa74cc787ce31009be64ddddd180bd897
Itay Zagron <zagron@gmail.com>
2022-05-17 20:07:34 +0300

    mreged with main

--- resources/provider/migrations/timescale/31_v0.11.6.down.sql
+++ resources/provider/migrations/timescale/31_v0.11.6.down.sql
@@ -13,9 +13,3 @@ DROP TABLE IF EXISTS aws_athena_work_group_prepared_statements;
 DROP TABLE IF EXISTS aws_athena_work_group_query_executions;
 DROP TABLE IF EXISTS aws_athena_work_group_named_queries;
 DROP TABLE IF EXISTS aws_athena_work_groups;
-
--- Resource: waf.web_acls
-DROP TABLE IF EXISTS aws_waf_web_acl_logging_configuration;
-
--- Resource: wafv2.web_acls
-DROP TABLE IF EXISTS aws_wafv2_web_acl_logging_configuration;
