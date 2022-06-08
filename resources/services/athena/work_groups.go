d5cdd8f130657816af307215b407401435fcef4b
bbernays <ben@bernays.me>
2022-06-08 11:51:20 -0400

    Update work_groups.go

--- resources/services/athena/work_groups.go
+++ resources/services/athena/work_groups.go
@@ -17,7 +17,7 @@ func WorkGroups() *schema.Table {
 		Name:        "aws_athena_work_groups",
 		Description: "A workgroup, which contains a name, description, creation time, state, and other configuration, listed under WorkGroup$Configuration",
 		Resolver: func(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- interface{}) error {
-			err := diag.WrapError(client.ListAndDetailResolver(ctx, meta, res, listWorkGroups, getWorkGroupDetail))
+			err := diag.WrapError(client.ListAndDetailResolver(ctx, meta, res, listWorkGroups, workGroupDetail))
 			return err
 		},
 		Multiplex:    client.ServiceAccountRegionMultiplexer("athena"),
@@ -560,7 +560,7 @@ func fetchAthenaWorkGroupNamedQueries(ctx context.Context, meta schema.ClientMet
 //                                                  User Defined Helpers
 // ====================================================================================================================
 
-func getWorkGroupDetail(ctx context.Context, meta schema.ClientMeta, resultsChan chan<- interface{}, errorChan chan<- error, summary interface{}) {
+func workGroupDetail(ctx context.Context, meta schema.ClientMeta, resultsChan chan<- interface{}, errorChan chan<- error, summary interface{}) {
 	c := meta.(*client.Client)
 	svc := c.Services().Athena
 	wg := summary.(types.WorkGroupSummary)
@@ -573,7 +573,6 @@ func getWorkGroupDetail(ctx context.Context, meta schema.ClientMeta, resultsChan
 		errorChan <- err
 	}
 	resultsChan <- *dc.WorkGroup
-
 }
 
 func createWorkGroupArn(cl *client.Client, groupName string) string {
