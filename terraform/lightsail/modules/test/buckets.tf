e9f12e87cf89d6608e70669617c5c2ae1efceb5c
Herman Schaaf <hermanschaaf@gmail.com>
2022-07-01 15:54:08 +0100

    Undo terraform changes (moved to different branch)

--- terraform/lightsail/modules/test/buckets.tf
+++ terraform/lightsail/modules/test/buckets.tf
@@ -1,4 +1,4 @@
 resource "awslightsail_bucket" "awslightsail_bucket" {
-  name      = "${lower(var.prefix)}-lightsail-bucket"
+  name      = "${var.prefix}-lightsail-bucket"
   bundle_id = "small_1_0"
 }
--- terraform/lightsail/modules/test/lb.tf
+++ terraform/lightsail/modules/test/lb.tf
@@ -1,5 +1,5 @@
 resource "awslightsail_lb" "awslightsail_lb" {
-  name              = "${var.prefix}_load_balancer"
+  name              = "${var.prefix}_load_ballancer"
   health_check_path = "/"
   instance_port     = "80"
 }
