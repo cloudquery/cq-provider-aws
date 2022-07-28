f827e782416bfe21b72e2b651b304d3fc516c3a1
Timur Irmatov <irmatov@gmail.com>
2022-07-28 14:34:49 +0300

    chore: Adjustments to trigger tf

--- terraform/glue/modules/test/trigger.tf
+++ terraform/glue/modules/test/trigger.tf
@@ -1,15 +1,9 @@
 resource "aws_glue_trigger" "example" {
   name = "${var.prefix}-glue-trigger"
-  type = "CONDITIONAL"
+  type = "ON_DEMAND"
+  description = "test trigger"
 
   actions {
     job_name = aws_glue_job.example.name
-  }
-
-  predicate {
-    conditions {
-      job_name = aws_glue_job.example.name
-      state    = "SUCCEEDED"
-    }
   }
 }
