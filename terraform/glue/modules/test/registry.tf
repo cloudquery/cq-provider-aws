ef78343f5cc2b12256c1ae6e180f680a29425215
Timur Irmatov <irmatov@gmail.com>
2022-07-29 13:49:15 +0300

    chore: Adjust Glue registries tf

--- terraform/glue/modules/test/registry.tf
+++ terraform/glue/modules/test/registry.tf
@@ -1,11 +1,19 @@
 resource "aws_glue_registry" "aws_glue_registry" {
   registry_name = "${var.prefix}-glue-registry"
+  description = "test registry"
+  tags = {
+    "testkey" = "somevalue"
+  }
 }
 
 resource "aws_glue_schema" "aws_glue_schema" {
-  schema_name       = "example"
+  schema_name       = "${var.prefix}-glue-schema"
   registry_arn      = aws_glue_registry.aws_glue_registry.arn
   data_format       = "AVRO"
   compatibility     = "NONE"
   schema_definition = "{\"type\": \"record\", \"name\": \"r1\", \"fields\": [ {\"name\": \"f1\", \"type\": \"int\"}, {\"name\": \"f2\", \"type\": \"string\"} ]}"
+  description       = "test schema"
+  tags = {
+    "testkey" = "othervalue"
+  }
 }
